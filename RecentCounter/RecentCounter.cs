369b9ce0d7e563d8a4bb2ecbcab72050e507e750
Julian Homez <julianhomesdev@icloud.com>
2025-08-23 22:11:19 -0500

    Thinking in RecentCounter

--- RecentCounter/RecentCounter.cs
+++ RecentCounter/RecentCounter.cs
@@ -1,11 +1,22 @@
-public class RecentCounter {
 
-    public RecentCounter() {
-        
+public class RecentCounter
+{
+
+
+    // Valid time range [t-3000, t]
+    // t -> time in miliseconds in the actually call
+    // t - 3000 -> low limit of the range
+    public RecentCounter()
+    {
+
+        int requests = 0;
+
     }
-    
-    public int Ping(int t) {
-        
+
+    public int Ping(int t)
+    {
+        // Initialize the counter
+        RecentCounter();
     }
 }
 
